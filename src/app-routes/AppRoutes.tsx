6a2494ec96a665a4bf34136b2b347e2872e3391f
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-05-01 05:31:42 +0530

    feat: added protected routes

--- src/app-routes/AppRoutes.tsx
+++ src/app-routes/AppRoutes.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import {
   Home,
@@ -8,19 +9,40 @@ import {
   QuestionPage,
   Result,
   Category,
+  Profile,
 } from "../pages";
+import { authContext } from "../context";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 function AppRoutes() {
+  const {
+    authState: {
+      userDetails: { token },
+    },
+  } = useContext(authContext);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/rules" element={<Rules />} />
-      <Route path="/question" element={<QuestionPage />} />
-      <Route path="/result" element={<Result />} />
       <Route path="/categories" element={<Category />} />
 
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
+      {!token ? (
+        <>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+        </>
+      ) : (
+        <>
+          <Route path="/signup" element={<Navigate replace to="/" />}></Route>
+          <Route path="/signin" element={<Navigate replace to="/" />}></Route>
+        </>
+      )}
+
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/question" element={<QuestionPage />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
 
       <Route path="*" element={<NotFound />} />
     </Routes>
